dfd801f739f06500382adf99418121df891f2a4e
Corey <corey@Coreys-MacBook-Pro.local>
2016-07-20 02:17:19 -0400

    Tests in ES6. Replace new Scale with Scale.create

--- src/scale.js
+++ src/scale.js
@@ -1,73 +1,121 @@
-import {shiftNotes, circleOfFifths, scaleNames, majorKeys, minorKeys, isMusicNote} from "./base";
+import {
+  shiftNotes,
+  circleOfFifths,
+  scaleNames,
+  majorKeys,
+  minorKeys,
+  isMusicNote
+}
+from "./base";
 import _ from "lodash";
 
-function Scale(config) {
-  if (!(this instanceof Scale))
-    return new Scale(config);
-  if (config === undefined || config.tonic === undefined || config.scale === undefined)
-    throw new Error("Invalid configuration supplied");
-  if (!isMusicNote(config.tonic))
-    throw new Error("Invalid tonic note supplied");
-  if(typeof config.scale !== "string" || !_.includes(scaleNames, config.scale.toUpperCase()))
-    throw new Error("Invalid scale provided");
+let ScaleUtilities = {
+  triad(index) {
+    return _chord(this, index, 5);
+  },
+  seventh(index) {
+    return _chord(this, index, 7);
+  }
+};
 
-  let _notes = _constructScale(config.tonic, config.scale);
+let Scale = {
+  create: function(config) {
+    let {error} = validateConf(config);
+    if (error.length)
+      return {
+        error
+      };
+    else {
+      let {
+        tonic,
+        scale
+      } = config;
+      let scaleObject = {
+        tonic,
+        scale,
+        notes: _constructScale(tonic, scale),
+        accidentals: getNumberOfAccidents(tonic, scale)
+      };
+      return Object.assign(scaleObject, ScaleUtilities);
+    }
+  }
+};
 
-  Object.defineProperty(this, "scale", {value: config.scale, enumerable: true});
-  Object.defineProperty(this, "notes", {value: _notes, enumerable: true});
-  Object.defineProperty(this, "accidentals", {value: getNumberOfAccidents(config.tonic, config.scale), enumerable: true});
-  Object.defineProperty(this, "tonic", {value: _notes[0], enumerable: true});
-  // Object.defineProperty(this, "seventh", {value: seventh, enumerable: true})
-  // Object.defineProperty(this, "triad", {value: triad, enumerable: true})
+function validateConf(config) {
+  let error = [];
+  if (!config)
+    error.push("Config object not specified");
+  else {
+    let {tonic ,scale } = config;
+    [validTonic(tonic), validScale(scale)].reduce((prev, cur) => {
+      if (cur.error)
+        prev.push(cur.error);
+      return prev;
+    }, error);
+  }
+
+  return { error };
 }
 
-Scale.prototype.triad = function (index){
-  return _chord(this, index, 5);
-};
-Scale.prototype.seventh = function (index){
-  return _chord(this, index, 7);
-};
+function validTonic(tonic) {
+  if (tonic) {
+    return _.includes(Object.keys(majorKeys), tonic) || _.includes(Object.keys(minorKeys), tonic);
+  } else {
+    return {
+      error: `${tonic} not valid scale tonic`
+    };
+  }
+}
+
+function validScale(scale) {
+  if (scale) {
+    return _.includes(scaleNames, scale.toUpperCase());
+  } else {
+    return {
+      error: `${scale} not a composable scale`
+    };
+  }
+}
 
 function _constructScale(tonic, scale) {
   let notes = shiftNotes(tonic);
   let numberofAccidents = scale.toUpperCase() === "MAJOR" ? majorKeys[tonic] : minorKeys[tonic];
   let currentAccidentIndex = numberofAccidents > 0 ? 0 : 6;
 
-  switch(numberofAccidents > 0)
-    {
-        //#'s
-      case true:
-        for (; numberofAccidents !== 0; numberofAccidents--, currentAccidentIndex++) {
-          let cur = notes.indexOf(circleOfFifths[currentAccidentIndex]);
-          notes[cur] = `${notes[cur]}#`;
-        }
-        break;
-        //b's
-      case false:
-        for (; numberofAccidents !== 0; numberofAccidents++, currentAccidentIndex--) {
-          let cur = notes.indexOf(circleOfFifths[currentAccidentIndex]);
-          notes[cur] = `${notes[cur]}b`;
-        }
-        break;
-        //C Major / A Minor
-      default:
-        break;
-    }
+  switch (numberofAccidents > 0) {
+    //#'s
+    case true:
+      for (; numberofAccidents !== 0; numberofAccidents--, currentAccidentIndex++) {
+        let cur = notes.indexOf(circleOfFifths[currentAccidentIndex]);
+        notes[cur] = `${notes[cur]}#`;
+      }
+      break;
+      //b's
+    case false:
+      for (; numberofAccidents !== 0; numberofAccidents++, currentAccidentIndex--) {
+        let cur = notes.indexOf(circleOfFifths[currentAccidentIndex]);
+        notes[cur] = `${notes[cur]}b`;
+      }
+      break;
+      //C Major / A Minor
+    default:
+      break;
+  }
   return notes;
 }
 
-function getNumberOfAccidents(tonic, scale){
+function getNumberOfAccidents(tonic, scale) {
   if (scaleNames.indexOf(scale.toUpperCase()) === -1)
     return;
   return scale.toUpperCase() === "MAJOR" ? majorKeys[tonic] : minorKeys[tonic];
 }
 
-function _chord(scaleObject, degree, range){
-  if (degree >= 0 && degree <= 6){
-    return _.range(degree, degree + range,2)
-              .map(item=>item%7)
-                .map(index => scaleObject.notes[index]);
+function _chord(scaleObject, degree, range) {
+  if (degree >= 0 && degree <= 6) {
+    return _.range(degree, degree + range, 2)
+      .map(item => item % 7)
+      .map(index => scaleObject.notes[index]);
   }
 }
 
-export {Scale, _constructScale};
+export {Scale, ScaleUtilities,_constructScale};
--- test/scale-test.js
+++ test/scale-test.js
@@ -1,22 +1,23 @@
 var expect = require("chai").expect;
-var Scale = require("../lib/scale").Scale;
+var {Scale, ScaleUtilities} = require("../lib/scale");
 
 describe("Scale", function() {
-  describe("Constructor", function() {
-    it("Should throw Error (no configuration object specified)", function() {
-      expect(Scale).to.throw(Error);
+  describe("Factory", function() {
+    it("Should throw object with error prop", () => {
+      let obj = Scale.create();
+      expect(obj).to.have.property('error')
     });
   });
 
   describe("Parameterized Constructor", function() {
-    it("Should return new Scale object in the given key", function() {
-
-      expect(new Scale({tonic: "A", scale: "Minor"})).to.eql(Object.assign({
+    it("Should return new Scale object in the given key", () => {
+      let obj = Scale.create({tonic: "A", scale: "Minor"});
+      expect(obj).to.eql(Object.assign({
         notes: ["A", "B", "C", "D", "E", "F", "G"],
         accidentals: 0,
         scale: "Minor",
         tonic: "A"
-      }, Scale.prototype));
+      },ScaleUtilities));
     });
   });
 });
